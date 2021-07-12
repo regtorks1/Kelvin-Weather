fdd039b38587d2c0dd84a224ca644add2ebaad4d
regtorks1 <reginald.torkonoo@amalitech.org>
2021-07-12 13:08:29 +0100

    added newton calculation

--- kelvin.js
+++ kelvin.js
@@ -7,3 +7,7 @@ let fahrenheit = celsius * (9/5) + 32;
 //rounding down the fahrenheit number
 fahrenheit = Math.floor(fahrenheit);
 console.log(`The temperature is ${fahrenheit} degrees Fahrenheit.`);
+// converting celcius to newton
+let Newton = celsius * (33/100);
+Newton = Math.floor(Newton);
+console.log(`The tempaerature is ${Newton} degrees Newton.`);
